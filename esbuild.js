eb7758db3c0a94694c550081ffc31c55c0806544
calebswank11 <calebswank@gmail.com>
2025-04-16 21:09:03 -0500

    Debugging for npm repo

--- esbuild.js
+++ esbuild.js
@@ -1,6 +1,29 @@
 const esbuild = require('esbuild');
 const fs = require('fs');
 
+const external = [
+  // 'tedious',
+  // 'better-sqlite3',
+  // 'mysql2',
+  // 'mysql',
+  // 'sqlite3',
+  // 'pg-query-stream',
+  // 'oracledb',
+  // 'prettier',
+  // '@aws-sdk/client-dynamodb',
+  // '@aws-sdk/client-lambda',
+  // '@aws-sdk/client-s3',
+  // '@aws-sdk/lib-dynamodb',
+  // allegedly, these are included in lambda runtime.
+  // '@aws-sdk/client-s3',
+  // '@aws-sdk/client-secrets-manager',
+  // '@aws-sdk/client-sts',
+  // '@aws-sdk/lib-storage',
+  // '@aws-sdk/s3-request-presigner',
+  // '@mapbox',
+  // 'knex',
+];
+
 const entryPointsConfig = [
   {
     entryPoints: ['./src/cli'],
@@ -13,11 +36,21 @@ const build = async () => {
   for (const config of entryPointsConfig) {
     fs.mkdirSync(config.outdir, { recursive: true });
     const buildConfigBase = {
-      entryPoints: ['./src/index.ts'],
-      outfile: 'dist/cli.js',
+      entryPoints: config.entryPoints,
+      // outfile: path.join(config.outdir, config.outfile),
+      outdir: config.outdir,
+      // entryNames: 'index',
+      outExtension: { '.js': '.js' },
+      // write: false,
+      // base config
       bundle: true,
+      minify: true,
       platform: 'node',
-      target: 'node18',
+      target: 'es2020',
+      external,
+      sourcemap: config.sourceMap,
+      logLevel: 'info',
+      treeShaking: true,
       format: 'cjs',
       banner: {
         js: '#!/usr/bin/env node',
